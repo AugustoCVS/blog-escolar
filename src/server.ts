ffd8a939b84678abf2368da23ba307cb279eef8d
AugustoCVS <guty.vertolis@gmail.com>
2025-01-04 21:29:05 -0300

    config: initial commit

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import "express-async-errors"
+import express, { Request, Response, NextFunction } from "express";
+import cors, { CorsOptions } from "cors";
+
+import { router } from "./api/routes/routes";
+
+const corsOptions: CorsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+}
+
+const app = express();
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(router);
+
+app.use((error: any, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return response.status(400).json({
+      error: error.message
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+})
+
+app.listen(3001, () => console.log("Rodando projeto na porta 3001"));
