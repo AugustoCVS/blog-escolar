88e4c4218a367972bb21f03ca39729f4c1c55021
AugustoCVS <guty.vertolis@gmail.com>
2025-01-18 13:29:00 -0300

    chore: update route name

--- src/api/routes/posts/posts-routes.ts
+++ src/api/routes/posts/posts-routes.ts
@@ -7,7 +7,7 @@ const postsController = new PostsController();
 
 postsRoutes.get("/list", postsController.getPosts.bind(postsController));
 postsRoutes.get("/list/:id", postsController.getPostById.bind(postsController));
-postsRoutes.get("/seatch", postsController.getPostsBySearch.bind(postsController));
+postsRoutes.get("/search", postsController.getPostsBySearch.bind(postsController));
 
 postsRoutes.get("/author/:authorId", ensureIsAdmin, postsController.getPostsByAuthor.bind(postsController));
 postsRoutes.post("/", ensureIsAdmin, postsController.createPost.bind(postsController));
