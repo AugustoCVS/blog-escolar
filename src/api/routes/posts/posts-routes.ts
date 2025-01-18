2b4496902b075bef1425fad8070ffd0e097564ba
AugustoCVS <guty.vertolis@gmail.com>
2025-01-18 12:46:08 -0300

    chore: update routes

--- src/api/routes/posts/posts-routes.ts
+++ src/api/routes/posts/posts-routes.ts
@@ -5,10 +5,11 @@ import { ensureIsAdmin } from "../../../middlewares/ensure-admin";
 const postsRoutes = Router();
 const postsController = new PostsController();
 
-postsRoutes.get("/", postsController.getPosts.bind(postsController));
-postsRoutes.get("/:id", postsController.getPosts.bind(postsController));
-postsRoutes.get("/search/list", postsController.getPosts.bind(postsController));
+postsRoutes.get("/list", postsController.getPosts.bind(postsController));
+postsRoutes.get("/list/:id", postsController.getPostById.bind(postsController));
+postsRoutes.get("/seatch", postsController.getPostsBySearch.bind(postsController));
 
+postsRoutes.get("/author/:authorId", ensureIsAdmin, postsController.getPostsByAuthor.bind(postsController));
 postsRoutes.post("/", ensureIsAdmin, postsController.createPost.bind(postsController));
 postsRoutes.put("/:id", ensureIsAdmin, postsController.updatePost.bind(postsController));
 postsRoutes.delete("/:id", ensureIsAdmin, postsController.deletePost.bind(postsController));
